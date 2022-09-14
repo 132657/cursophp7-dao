54d40bd751228e1ddb40367c23b454c28103af52
Oi-Daniel <88396901+132657@users.noreply.github.com>
2022-09-14 17:26:38 -0300

    inserir e alterar

--- index.php
+++ index.php
@@ -17,5 +17,23 @@ require_once("config.php");
 //echo json_encode($lista);
 
 //Carrega uma lista de usuarios buscando pelo login
-$search = Usuario::search()
- ?>
+//$search = Usuario::search("r");
+//echo json_encode($search);
+
+//Carrega um usuario usando o login e a senha
+//$usuario = new Usuario();
+//$usuario->login("MicheleMoura", "michele123456");
+//echo $usuario;
+
+/* Criando um novo usuario
+$aluno = new Usuario("daniel", "jhgadjh");
+$aluno->insert ();
+echo $aluno;
+*/
+
+$usuario = new Usuario();
+$usuario->loadById(8);
+$usuario->update("instrutor", "hjdfgbjahsdb");
+echo $usuario;
+
+ ?> 
